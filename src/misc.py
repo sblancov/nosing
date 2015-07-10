702bde9e43aa3c0184c05f766f61036ec17deff3
Santiago Blanco <sblancov84@gmail.com>
2015-07-10 23:58:13 +0200

    Add documentation

--- src/misc.py
+++ src/misc.py
@@ -1,24 +1,38 @@
 class StringSet(object):
+    ''' This class allows to parse a list of comma separated strings '''
 
     def __init__(self, collection):
+        ''' Constructor
+            - collection: It has to be a set.
+        '''
         if not isinstance(collection, set):
             raise TypeError("collection parameter should be a set instance")
         self.collection = collection
 
     def add(self, string):
+        '''Receives 'string', it is stripped and added to the list
+            - string: A string that is stripped before to be added to list
+        '''
         self.__add_strip_string(string)
 
     def remove(self, string):
+        ''' Remove 'string' from the list
+            - string: A string that will be remove if it exists
+        '''
         strip_string = string.strip()
         if strip_string in self.collection:
             self.collection.remove(strip_string)
 
     def parse(self, string):
+        ''' Receives a list of comma separated strings and process it
+            - string: A string that contains comma separated strings
+        '''
         split_string = string.split(',')
         for item in split_string:
             self.__add_strip_string(item)
 
     def clean(self):
+        ''' Remove all the strings '''
         self.collection.clear()
 
     def __iter__(self):
